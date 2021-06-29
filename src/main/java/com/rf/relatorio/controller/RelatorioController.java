97941c778c453112f55fb5d29986966b24787e05
RamonFern <ramonfernandes9@gmail.com>
2021-06-29 15:24:03 -0300

    Entidade Relatório criada e lombok funcionando

--- src/main/java/com/rf/relatorio/controller/RelatorioController.java
+++ src/main/java/com/rf/relatorio/controller/RelatorioController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rf.relatorio.entity.Relatorio;
+
 @RestController
 @RequestMapping("/api/v1/relatorio")
 public class RelatorioController {
 
 	@GetMapping
 	public String getRelatorio() {
-		return "Teste API relatorio...";
+		Relatorio relatorio = new Relatorio();
+		relatorio.setInspetor("Rodrigo");
+		relatorio.setTexto("relatorio diário de serviço");
+		return "Teste API relatório dmt "+ relatorio.getTexto();
+		
 	}
 }
